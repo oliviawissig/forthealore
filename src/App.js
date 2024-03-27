ba283cfc5ce94cddfb388fbf8948744ead3188bf
oliviawissig <oliviawissig@gmail.com>
2024-03-26 21:23:03 -0700

    updated styling and navbar

--- src/App.js
+++ src/App.js
@@ -16,8 +16,8 @@ import xavier from "./images/xavier.png";
 import shmunty from "./images/shmunty.png";
 import jcash from "./images/jcash.png";
 import shino from "./images/shino.png";
-import matthew from './images/matthew.PNG'
-import jason from './images/jason.jpg'
+import matthew from "./images/matthew.PNG";
+import jason from "./images/jason.jpg";
 import logo from "./images/alore-logo.PNG";
 
 import { useEffect, useState } from "react";
@@ -31,88 +31,14 @@ function App() {
 
 	return (
 		<div className="App">
-			<div className="Info">
-				<div className="Header">
-					<img
-						src={logo}
-						className={loaded ? "logo" : "logo-unloaded"}
-						alt="alore logo"
-						width="175"
-					/>
-				</div>
-				<h3>alore radio:</h3>
-				<div className="iframe-container">
-					<iframe className="iframe-responsive" width="560" height="315" src="https://www.youtube.com/embed/BHMAGWTIgcc?si=_IsS5Zsf7QTo7P9y" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-				</div>
-				<h3>meet the team:</h3>
-				<div className="Team">
-					<TeamCard
-						memberName="SHINO SMOKE"
-						memberImg={shino}
-						href="https://www.instagram.com/shinosmoke/"
-						className={loaded ? "TeamImg" : "TeamImg-unloaded"}
-						width="720"
-						height="1077"
-					/>
-					<TeamCard
-						memberName="SOLACE DENIM"
-						memberImg={solace}
-						href="https://www.instagram.com/solacedenim/"
-						className={loaded ? "TeamImg" : "TeamImg-unloaded"}
-						width="720"
-						height="1077"
-					/>
-					<TeamCard
-						memberName="MISS ORGANIC"
-						memberImg={organic}
-						href="https://www.instagram.com/ms.orgaanic/"
-						className={loaded ? "TeamImg" : "TeamImg-unloaded"}
-						width="596"
-						height="883"
-					/>
-					<TeamCard
-						memberName="TWOSEVEN"
-						memberImg={xavier}
-						href="https://www.instagram.com/2700jit/"
-						className={loaded ? "TeamImg" : "TeamImg-unloaded"}
-						width="614"
-						height="909"
-					/>
-					<TeamCard
-						memberName="SHMUNTY"
-						memberImg={shmunty}
-						href="https://www.instagram.com/shmunty/"
-						className={loaded ? "TeamImg" : "TeamImg-unloaded"}
-						width="585"
-						height="875"
-					/>
-					<TeamCard
-						memberName="JCASH"
-						memberImg={jcash}
-						href="https://www.instagram.com/jcashbta/"
-						className={loaded ? "TeamImg" : "TeamImg-unloaded"}
-						width="1312"
-						height="1968"
-					/>
-					<TeamCard
-						memberName="JASON"
-						memberImg={jason}
-						href="https://www.instagram.com/paduaudap/"
-						className={loaded ? "TeamImg" : "TeamImg-unloaded"}
-						width="768"
-						height="614"
-					/>
-					<TeamCard
-						memberName="MATTHEW"
-						memberImg={matthew}
-						href="https://www.instagram.com/jason__gtz/"
-						className={loaded ? "TeamImg" : "TeamImg-unloaded"}
-						width="1290"
-						height="2556"
-					/>
-				</div>
-				<h3>contact:</h3>
-				<Form />
+			<div className="Header">
+				<img
+					src={logo}
+					className={loaded ? "logo" : "logo-unloaded"}
+					alt="alore logo"
+					width="296"
+					height="208"
+				/>
 				<div className="Socials">
 					<a
 						className={
@@ -150,6 +76,91 @@ function App() {
 						<FontAwesomeIcon icon={faDiscord} size="sm" />
 					</a>
 				</div>
+			</div>
+
+			<div className="site-body">
+				<h3>alore radio:</h3>
+				<div className="iframe-container">
+					<iframe
+						className="iframe-responsive"
+						width="560"
+						height="315"
+						src="https://www.youtube.com/embed/BHMAGWTIgcc?si=_IsS5Zsf7QTo7P9y"
+						title="YouTube video player"
+						frameborder="0"
+						allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+						referrerpolicy="strict-origin-when-cross-origin"
+						allowfullscreen></iframe>
+				</div>
+				<h3>meet the team:</h3>
+				<div className="Team">
+					<TeamCard
+						memberName="Shino Smoke"
+						memberImg={shino}
+						href="https://www.instagram.com/shinosmoke/"
+						className={loaded ? "TeamImg" : "TeamImg-unloaded"}
+						width="720"
+						height="1077"
+					/>
+					<TeamCard
+						memberName="Solace Denim"
+						memberImg={solace}
+						href="https://www.instagram.com/solacedenim/"
+						className={loaded ? "TeamImg" : "TeamImg-unloaded"}
+						width="720"
+						height="1077"
+					/>
+					<TeamCard
+						memberName="Miss Organic"
+						memberImg={organic}
+						href="https://www.instagram.com/ms.orgaanic/"
+						className={loaded ? "TeamImg" : "TeamImg-unloaded"}
+						width="596"
+						height="883"
+					/>
+					<TeamCard
+						memberName="Two-Seven"
+						memberImg={xavier}
+						href="https://www.instagram.com/2700jit/"
+						className={loaded ? "TeamImg" : "TeamImg-unloaded"}
+						width="614"
+						height="909"
+					/>
+					<TeamCard
+						memberName="Shmunty"
+						memberImg={shmunty}
+						href="https://www.instagram.com/shmunty/"
+						className={loaded ? "TeamImg" : "TeamImg-unloaded"}
+						width="585"
+						height="875"
+					/>
+					<TeamCard
+						memberName="J Cash"
+						memberImg={jcash}
+						href="https://www.instagram.com/jcashbta/"
+						className={loaded ? "TeamImg" : "TeamImg-unloaded"}
+						width="1312"
+						height="1968"
+					/>
+					<TeamCard
+						memberName="Jason"
+						memberImg={jason}
+						href="https://www.instagram.com/paduaudap/"
+						className={loaded ? "TeamImg" : "TeamImg-unloaded"}
+						width="768"
+						height="614"
+					/>
+					<TeamCard
+						memberName="Matthew"
+						memberImg={matthew}
+						href="https://www.instagram.com/jason__gtz/"
+						className={loaded ? "TeamImg" : "TeamImg-unloaded"}
+						width="1290"
+						height="2556"
+					/>
+				</div>
+				<h3>contact:</h3>
+				<Form />
 			</div>
 		</div>
 	);
--- src/TeamCard.js
+++ src/TeamCard.js
@@ -5,11 +5,11 @@ function TeamCard({ href, memberName, memberImg, className  }) {
     return ( 
         <a href={href} className="team-card-container">
             <div className='card-img-container'>
-                <img src={memberImg} className={className} alt={"profile avatar of " + memberName} style={memberName === 'SHINO SMOKE' ? {alignSelf:'flex-start'} : {}}></img>
+                <img src={memberImg} className={className} alt={"profile avatar of " + memberName}></img>
             </div>
             <div className="card-body">
                 <p>{memberName}</p>
-                <a href='' className="card-link">Instagram &#8594;</a>
+                <a href='' className="card-link">Instagram <span className='card-arrow'>&#8594;</span></a>
             </div>
         </a>
      );
