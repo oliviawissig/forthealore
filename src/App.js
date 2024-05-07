e9c70da9fa11dba2d6e33eb7064c64139908663e
oliviawissig <oliviawissig@gmail.com>
2024-05-06 18:46:08 -0700

    updated alore live promo, added some lazy loading & pre-loading thumbnails

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import TeamCard from "./TeamCard";
 import Form from "./Form";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
@@ -21,144 +22,153 @@ function App() {
 	}, []);
 
 	return (
-		<div className="App">
-			<div className="Header">
-				<img
-					src="https://res.cloudinary.com/dyyurflew/image/upload/f_auto,q_auto/zmrvt05zz1t4oiswipv1"
-					className={loaded ? "logo" : "logo-unloaded"}
-					alt="alore logo"
-					width="296"
-					height="208"
-				/>
-				<div className="Socials">
-					<a
-						className={
-							loaded ? "socialIcon" : "socialIcon-unloaded"
-						}
-						href="https://www.instagram.com/forthealore/">
-						<FontAwesomeIcon size="sm" icon={faInstagram} />
-					</a>
-					<a
-						className={
-							loaded ? "socialIcon" : "socialIcon-unloaded"
-						}
-						href="https://www.tiktok.com/@forthealore">
-						<FontAwesomeIcon size="sm" icon={faTiktok} />
-					</a>
-					<a
-						className={
-							loaded ? "socialIcon" : "socialIcon-unloaded"
-						}
-						href="https://on.soundcloud.com/9kooV">
-						<FontAwesomeIcon icon={faSoundcloud} size="sm" />
-					</a>
-					<a
-						className={
-							loaded ? "socialIcon" : "socialIcon-unloaded"
-						}
-						href="https://www.youtube.com/@forthealore">
-						<FontAwesomeIcon icon={faYoutube} size="sm" />
-					</a>
-					<a
-						className={
-							loaded ? "socialIcon" : "socialIcon-unloaded"
-						}
-						href="https://discord.gg/HW2qtJvk8J">
-						<FontAwesomeIcon icon={faDiscord} size="sm" />
-					</a>
+		<HelmetProvider>
+			<div className="App">
+				<Helmet>
+					<meta name="title" content="ALORE"/>
+					<meta name="description" content="The landing site for all things ALORE"/>
+					<link rel='canonical' href='https://www.forthealore.com/' />
+				</Helmet>
+				<div className="Header">
+					<div className={loaded ? "blurred-logo-img-loaded" : "blurred-logo-img"}>
+						<img
+							src="https://res.cloudinary.com/dyyurflew/image/upload/f_auto,q_auto/zmrvt05zz1t4oiswipv1"
+							alt="alore logo"
+							width="296"
+							height="208"
+						/>
+					</div>
+					<h1 style={{display:"none"}}>welcome to the alore experience</h1>
+					<div className="Socials">
+						<a
+							className={
+								loaded ? "socialIcon" : "socialIcon-unloaded"
+							}
+							href="https://www.instagram.com/forthealore/">
+							<FontAwesomeIcon size="sm" icon={faInstagram} />
+						</a>
+						<a
+							className={
+								loaded ? "socialIcon" : "socialIcon-unloaded"
+							}
+							href="https://www.tiktok.com/@forthealore">
+							<FontAwesomeIcon size="sm" icon={faTiktok} />
+						</a>
+						<a
+							className={
+								loaded ? "socialIcon" : "socialIcon-unloaded"
+							}
+							href="https://on.soundcloud.com/9kooV">
+							<FontAwesomeIcon icon={faSoundcloud} size="sm" />
+						</a>
+						<a
+							className={
+								loaded ? "socialIcon" : "socialIcon-unloaded"
+							}
+							href="https://www.youtube.com/@forthealore">
+							<FontAwesomeIcon icon={faYoutube} size="sm" />
+						</a>
+						<a
+							className={
+								loaded ? "socialIcon" : "socialIcon-unloaded"
+							}
+							href="https://discord.gg/HW2qtJvk8J">
+							<FontAwesomeIcon icon={faDiscord} size="sm" />
+						</a>
+					</div>
+					{/* <div className="promo">
+						<img src="https://res.cloudinary.com/dyyurflew/image/upload/f_auto,q_auto/xnfmmfpgsqv6yrbk6lu5" alt="NOODZ promo" width="720" height="898" className="promo-img"/>
+						<a href="https://socialcluboftomorrow.com/" className="btn">Get Access</a>
+					</div> */}
 				</div>
-				{/* <div className="promo">
-					<img src="https://res.cloudinary.com/dyyurflew/image/upload/f_auto,q_auto/xnfmmfpgsqv6yrbk6lu5" alt="NOODZ promo" width="720" height="898" className="promo-img"/>
-					<a href="https://socialcluboftomorrow.com/" className="btn">Get Access</a>
-				</div> */}
-			</div>
 
-			<div className="site-body">
-				{/* <h3>alore live:</h3>
-				<div className="promo">
-					<a href="https://www.eventbrite.com/e/hyphy-house-tiger-friday-april-26th-tickets-883750209667?utm_experiment=test_share_listing&aff=ebdsshios">
-						<img src="https://res.cloudinary.com/dyyurflew/image/upload/f_auto,q_auto/alorehyphyhouse_fmyb3i" alt="hyphy house promo flyer" width="720" height="898" className="promo-img"/>
-					</a>
-					<a href="https://www.eventbrite.com/e/hyphy-house-tiger-friday-april-26th-tickets-883750209667?utm_experiment=test_share_listing&aff=ebdsshios" className="btn">Get Access</a>
-				</div> */}
+				<div className="site-body">
+					<h2>alore live:</h2>
+					<div className="promo">
+						<a href="https://www.eventbrite.com/e/alore-presents-the-klm-experience-tickets-899270210407?aff=oddtdtcreator">
+							<div className="blurred-promo-img">
+								<img src="https://res.cloudinary.com/dyyurflew/image/upload/f_auto,q_auto/eteu6ib69cczx9esqyzz" alt="alore LA promo flyer" width="720" height="898" className="promo-img"/>
+							</div>
+						</a>
+						<a href="https://www.eventbrite.com/e/alore-presents-the-klm-experience-tickets-899270210407?aff=oddtdtcreator" className="btn">Buy Tickets</a>
+					</div>
 
-				<h3>youtube channel:</h3>
-				<div className="iframe-container">
-				<iframe className="iframe-responsive"
-						width="560"
-						height="315" src="https://www.youtube.com/embed/rFOtFp8Gyrw?si=oCZKpN3Zm_9MCpDZ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+					<h2>youtube channel:</h2>
+					<div className="iframe-container">
+						<iframe className="iframe-responsive" width="560" height="315" src="https://www.youtube.com/embed/24CgikCK4uk?si=o5p5WX_YXrK4dFzx" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen loading="lazy"></iframe>
+					</div>
+					<h2>meet the team:</h2>
+					<div className="Team">
+						<TeamCard
+							memberName="Shino Smoke" memberId="shino"
+							memberImg="https://res.cloudinary.com/dyyurflew/image/upload/f_auto,q_auto/icabnapkiqf2fpf31tp4"
+							href="https://www.instagram.com/shinosmoke/"
+							className={loaded ? "TeamImg" : "TeamImg-unloaded"}
+							width="596"
+							height="737"
+						/>
+						<TeamCard
+							memberName="Solace Denim" memberId="solace"
+							memberImg="https://res.cloudinary.com/dyyurflew/image/upload/f_auto,q_auto/obcifi92l2zt7sdpx6ks"
+							href="https://www.instagram.com/solacedenim/"
+							className={loaded ? "TeamImg" : "TeamImg-unloaded"}
+							width="720"
+							height="1077"
+						/>
+						<TeamCard
+							memberName="Miss Organic" memberId="organic"
+							memberImg="https://res.cloudinary.com/dyyurflew/image/upload/f_auto,q_auto/ygbgjynwb3lq9y3akrnp"
+							href="https://www.instagram.com/ms.orgaanic/"
+							className={loaded ? "TeamImg" : "TeamImg-unloaded"}
+							width="596"
+							height="881"
+						/>
+						<TeamCard
+							memberName="Two-Seven" memberId="twoseven"
+							memberImg="https://res.cloudinary.com/dyyurflew/image/upload/f_auto,q_auto/bywgph8iwszbi7zc1nvu"
+							href="https://www.instagram.com/x8v1er/"
+							className={loaded ? "TeamImg" : "TeamImg-unloaded"}
+							width="614"
+							height="909"
+						/>
+						<TeamCard
+							memberName="Shmunty" memberId="shmunty"
+							memberImg="https://res.cloudinary.com/dyyurflew/image/upload/f_auto,q_auto/gzktarac5tocf6ldojuj"
+							href="https://www.instagram.com/shmunty/"
+							className={loaded ? "TeamImg" : "TeamImg-unloaded"}
+							width="585"
+							height="875"
+						/>
+						<TeamCard
+							memberName="J Cash" memberId="jcash"
+							memberImg="https://res.cloudinary.com/dyyurflew/image/upload/f_auto,q_auto/vxjbrsiz8q2rictewbmq"
+							href="https://www.instagram.com/jcashbta/"
+							className={loaded ? "TeamImg" : "TeamImg-unloaded"}
+							width="1312"
+							height="1968"
+						/>
+						<TeamCard
+							memberName="Jason" memberId="jason"
+							memberImg="https://res.cloudinary.com/dyyurflew/image/upload/f_auto,q_auto/enacnnihra8x43jsnvpf"
+							href="https://www.instagram.com/jason__gtz/"
+							className={loaded ? "TeamImg" : "TeamImg-unloaded"}
+							width="768"
+							height="614"
+						/>
+						<TeamCard
+							memberName="Matthew" memberId="matthew"
+							memberImg="https://res.cloudinary.com/dyyurflew/image/upload/f_auto,q_auto/x2l5mbefnimugmz2wufn"
+							href="https://www.instagram.com/paduaudap/"
+							className={loaded ? "TeamImg" : "TeamImg-unloaded"}
+							width="1290"
+							height="2556"
+						/>
+					</div>
+					<h2>contact:</h2>
+					<Form />
 				</div>
-				<h3>meet the team:</h3>
-				<div className="Team">
-					<TeamCard
-						memberName="Shino Smoke"
-						memberImg="https://res.cloudinary.com/dyyurflew/image/upload/f_auto,q_auto/icabnapkiqf2fpf31tp4"
-						href="https://www.instagram.com/shinosmoke/"
-						className={loaded ? "TeamImg" : "TeamImg-unloaded"}
-						width="720"
-						height="1077"
-					/>
-					<TeamCard
-						memberName="Solace Denim"
-						memberImg="https://res.cloudinary.com/dyyurflew/image/upload/f_auto,q_auto/obcifi92l2zt7sdpx6ks"
-						href="https://www.instagram.com/solacedenim/"
-						className={loaded ? "TeamImg" : "TeamImg-unloaded"}
-						width="720"
-						height="1077"
-					/>
-					<TeamCard
-						memberName="Miss Organic"
-						memberImg="https://res.cloudinary.com/dyyurflew/image/upload/f_auto,q_auto/ygbgjynwb3lq9y3akrnp"
-						href="https://www.instagram.com/ms.orgaanic/"
-						className={loaded ? "TeamImg" : "TeamImg-unloaded"}
-						width="596"
-						height="883"
-					/>
-					<TeamCard
-						memberName="Two-Seven"
-						memberImg="https://res.cloudinary.com/dyyurflew/image/upload/f_auto,q_auto/bywgph8iwszbi7zc1nvu"
-						href="https://www.instagram.com/x8v1er/"
-						className={loaded ? "TeamImg" : "TeamImg-unloaded"}
-						width="614"
-						height="909"
-					/>
-					<TeamCard
-						memberName="Shmunty"
-						memberImg="https://res.cloudinary.com/dyyurflew/image/upload/f_auto,q_auto/gzktarac5tocf6ldojuj"
-						href="https://www.instagram.com/shmunty/"
-						className={loaded ? "TeamImg" : "TeamImg-unloaded"}
-						width="585"
-						height="875"
-					/>
-					<TeamCard
-						memberName="J Cash"
-						memberImg="https://res.cloudinary.com/dyyurflew/image/upload/f_auto,q_auto/vxjbrsiz8q2rictewbmq"
-						href="https://www.instagram.com/jcashbta/"
-						className={loaded ? "TeamImg" : "TeamImg-unloaded"}
-						width="1312"
-						height="1968"
-					/>
-					<TeamCard
-						memberName="Jason"
-						memberImg="https://res.cloudinary.com/dyyurflew/image/upload/f_auto,q_auto/enacnnihra8x43jsnvpf"
-						href="https://www.instagram.com/jason__gtz/"
-						className={loaded ? "TeamImg" : "TeamImg-unloaded"}
-						width="768"
-						height="614"
-					/>
-					<TeamCard
-						memberName="Matthew"
-						memberImg="https://res.cloudinary.com/dyyurflew/image/upload/f_auto,q_auto/x2l5mbefnimugmz2wufn"
-						href="https://www.instagram.com/paduaudap/"
-						className={loaded ? "TeamImg" : "TeamImg-unloaded"}
-						width="1290"
-						height="2556"
-					/>
-				</div>
-				<h3>contact:</h3>
-				<Form />
 			</div>
-		</div>
+		</HelmetProvider>
 	);
 }
 
--- src/TeamCard.js
+++ src/TeamCard.js
@@ -1,11 +1,26 @@
 import './TeamCard.css';
 
-function TeamCard({ href, memberName, memberImg, className  }) {
+const team_lazy_load_imgs = {
+    shino: 'https://res.cloudinary.com/dyyurflew/image/upload/c_scale,h_0.10,w_0.10/icabnapkiqf2fpf31tp4.jpg',
+    solace: 'https://res.cloudinary.com/dyyurflew/image/upload/c_scale,h_0.10,w_0.10/obcifi92l2zt7sdpx6ks.jpg', 
+    organic: 'https://res.cloudinary.com/dyyurflew/image/upload/c_scale,h_0.10,w_0.10/ygbgjynwb3lq9y3akrnp.jpg', 
+    twoseven: 'https://res.cloudinary.com/dyyurflew/image/upload/c_scale,h_0.10,w_0.10/bywgph8iwszbi7zc1nvu.jpg', 
+    shmunty: 'https://res.cloudinary.com/dyyurflew/image/upload/c_scale,h_0.10,w_0.10/gzktarac5tocf6ldojuj.jpg', 
+    jcash: 'https://res.cloudinary.com/dyyurflew/image/upload/c_scale,h_0.10,w_0.10/vxjbrsiz8q2rictewbmq.jpg', 
+    jason: 'https://res.cloudinary.com/dyyurflew/image/upload/c_scale,h_0.10,w_0.10/v1711513852/enacnnihra8x43jsnvpf.jpg',
+    matthew: 'https://res.cloudinary.com/dyyurflew/image/upload/c_scale,h_0.10,w_0.10/v1711513865/x2l5mbefnimugmz2wufn.png'
+}
+
+function TeamCard({ href, memberName, memberImg, memberId,  className, width, height  }) {
+    const team_lazy_load_url = team_lazy_load_imgs[memberId];
+    console.log(team_lazy_load_url)
 
     return ( 
         <a href={href} className="team-card-container">
             <div className='card-img-container'>
-                <img src={memberImg} className={className} alt={"profile avatar of " + memberName}></img>
+                <div className='blurred-team-img' style={{backgroundImage: 'url(' + team_lazy_load_url + ')'}}>
+                    <img src={memberImg} className={className} alt={"profile avatar of " + memberName} width={width} height={height} loading="lazy"></img>
+                </div>
             </div>
             <div className="card-body">
                 <p style={{fontSize:'1.2rem'}}>{memberName}</p>
